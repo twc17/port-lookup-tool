6addf28865c407e254c8baaa892c23e0955aa6c6
Troy <twc17@pitt.edu>
2016-10-13 14:34:20 -0400

    Changed packages to install with pip3

--- extras/bootstrap.sh
+++ extras/bootstrap.sh
@@ -6,20 +6,11 @@ apt-get update
 # Install Python3 and Paramiko lib
 apt-get install -y build-essential libssl-dev libffi-dev python-dev
 apt-get install -y python3
-apt-get install -y python-pip
-apt-get install -y ipython
+apt-get install -y python3-pip
 apt-get install -y libapache2-mod-python
 apt-get install -y libapache2-mod-python-doc
 # Paramiko dependencies
-pip install six
-pip install cryptography
-pip install paramiko
-
-# Copy Paramiko over to Python3 directory
-cp -r /usr/local/lib/python2.7/dist-packages/paramiko/ /usr/local/lib/python3.4/dist-packages/
-cp -r /usr/lib/python2.7/dist-packages/six.py /usr/local/lib/python3.4/dist-packages/
-cp -r /usr/local/lib/python2.7/dist-packages/cryptography/ /usr/local/lib/python3.4/dist-packages/
-cp -r /usr/local/lib/python2.7/dist-packages/pyasn1 /usr/local/lib/python3.4/dist-packages/
+pip3 install paramiko
 
 # Install Apache web server, and link /var/www to the shared /vagrant directory
 apt-get install -y apache2
@@ -37,7 +28,7 @@ cp /vagrant/extras/port-lookup-tool-apache.conf /etc/apache2/sites-available/por
 ln -fs /etc/apache2/sites-available/port-lookup-tool-apache.conf /etc/apache2/sites-enabled/port-lookup-tool-apache.conf
 
 # Load CGI module
-a2enmod cgi
+a2enmod cgid
 
 # Restart Apache
 service apache2 restart
